8d6d1f5baf18faf3802d98c7664d10aebfc50d62
landrea-velez <landrea.velez@gmail.com>
2015-02-01 11:36:39 -0500

    Uncommited changes

---
+++ Search.FFP/app/src/main/java/searchffp/compmovil/udea/edu/co/searchffp/MyPlaces.java
@@ -0,0 +1,12 @@
+package searchffp.compmovil.udea.edu.co.searchffp;
+
+import android.app.Activity;
+import android.widget.ListView;
+
+
+public class MyPlaces extends Activity {
+
+    private ListView list;
+    private String[] sistemas = {"Ubuntu", "Android", "iOS", "Windows", "Mac OSX",
+            "Google Chrome OS", "Debian", "Mandriva", "Solaris", "Unix"};
+}
